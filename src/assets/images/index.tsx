94df9ebd7b3fff1fb1acdd8e4a5617c23455ccd8
johnarcojada <johnarcojada@gmail.com>
2023-07-15 19:03:51 +0800

    feature # Create hero section

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -6,6 +6,16 @@ import IconHeadset from './nav-icons/icon-headset.svg';
 import IconSearch from './nav-icons/icon-search.svg';
 import IconUSFlag from './nav-icons/icon-us-flag.svg';
 import IconChevronRight from './icon-chevron-right.svg';
+import HeroBgDesktop from './hero-bg-desktop.png';
+import HeroPersonsBg from './hero-persons-bg.jpg';
+import HeroPromo from './hero-promo.png';
+import IconCrown from './icon-crown.svg';
+import IconStar from './icon-star.svg';
+import IconShipping from './icon-shipping.svg';
+import IconLocation from './icon-location.svg';
+import IconTag from './icon-tag.svg';
+import IconChevronLeftWhite from './icon-chevron-left-white.svg';
+import IconChevronRightWhite from './icon-chevron-right-white.svg';
 
 export {
   Logo,
@@ -16,4 +26,14 @@ export {
   IconSearch,
   IconUSFlag,
   IconChevronRight,
+  HeroBgDesktop,
+  HeroPersonsBg,
+  HeroPromo,
+  IconCrown,
+  IconStar,
+  IconShipping,
+  IconLocation,
+  IconTag,
+  IconChevronLeftWhite,
+  IconChevronRightWhite,
 };
