d3d1d903e72b2c0731d762f988119441b8de4be4
johnarcojada <johnarcojada@gmail.com>
2023-07-16 00:04:59 +0800

    feature # Create new arrivals

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from 'components/Navbar';
 import Deals from 'components/Deals';
 import VipExclusive from 'components/VipExclusive';
 import Trending from 'components/Trending';
+import NewArrivals from 'components/NewArrivals';
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
       <Deals />
       <VipExclusive />
       <Trending />
+      <NewArrivals />
     </>
   );
 };
