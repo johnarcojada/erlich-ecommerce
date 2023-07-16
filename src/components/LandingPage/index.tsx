00e5cc74e821bb29e0c08b805b492c392fcb909c
johnarcojada <johnarcojada@gmail.com>
2023-07-16 12:17:56 +0800

    feature # Add router

---
+++ src/components/LandingPage/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Hero from 'components/Hero';
+import Navbar from 'components/Navbar';
+import Deals from 'components/Deals';
+import VipExclusive from 'components/VipExclusive';
+import Trending from 'components/Trending';
+import NewArrivals from 'components/NewArrivals';
+import RecentlyBought from 'components/RecentlyBought';
+import Inspo from 'components/Inspo';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Deals />
+      <VipExclusive />
+      <Trending />
+      <NewArrivals />
+      <RecentlyBought />
+      <Inspo />
+    </>
+  );
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './index.scss';
+
+import CartPage from 'components/CartPage';
+import LandingPage from 'components/LandingPage';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage />,
+  },
+  {
+    path: 'cart',
+    element: <CartPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
